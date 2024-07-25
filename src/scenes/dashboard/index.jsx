d7aa618982a111ec662f7820184b17389835fb01
Ujjawal Kaul <ujjwal171@outlook.com>
2024-07-25 20:34:00 +0530

    Imports

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
 import Header from '../../components/Header';
-import { Box } from '@mui/material';
+import { Box, Button, IconButton, Typography, useTheme } from '@mui/material';
+import { tokens } from '../../theme';
+import { mockTransactions } from '../../data/mockData';
+import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
+import EmailIcon from '@mui/icons-material/Email';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import TrafficIcon from '@mui/icons-material/Traffic';
+import LineChart from '../../components/LineChart';
+import BarChart from '../../components/BarChart';
+import GeographyChart from '../../components/GeographyChart';
+import StatBox from '../../components/StatBox';
+import ProgressCircle from '../../components/ProgressCircle';
+
 export default function Dashboard() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
