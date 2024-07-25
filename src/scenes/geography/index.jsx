3b666fbddd16e067d90d31c67341a22fc7645933
Ujjawal Kaul <ujjwal171@outlook.com>
2024-07-25 20:05:19 +0530

    Added border around map

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, colors } from '@mui/material';
 import Header from '../../components/Header';
 import GeographyChart from '../../components/GeographyChart';
 
@@ -6,7 +6,10 @@ export default function Geography() {
   return (
     <Box m="20px">
       <Header title="Geography Chart" subtitle="Simple Geography Chart" />
-      <Box height="75vh">
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="8px">
         <GeographyChart />
       </Box>
     </Box>
